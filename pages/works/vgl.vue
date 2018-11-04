66d9cb03ec89a3601c1ba14cef0393fdc918e37a
Unknown <takuto.hamano@gmail.com>
2018-11-04 13:24:24 +0900

    Add the component of vgl.

---
+++ pages/works/vgl.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <work
+      :screen-shots="['/works/portfolio/figure0.png']"
+      :technologies="['C++', 'PhysX', 'OpenGL(GLSL)', 'WebSocket', 'Qt']"
+      :references="[
+        'Performance comparison of physics engines to accelerate house-collapsing simulations, Proc. of IEEE International Symposium on Safety, Security and Rescue Robotics, 2016',
+        '物理エンジンの性能評価に基づいたリアルタイム家屋倒壊シミュレーション, 日本機械学会論文集, 2018']"
+      title="Virtual Gareki Lab"
+      date="2015-2018"
+      description="仮想空間上で地震による家屋の倒壊をシミュレーションし，がれきモデルを生成するためのツール．シミュレーションには剛体物理エンジンとしてNVIDIA社のPhysX 3.4を利用し，数千～数万個の剛体から構成されるシミュレーション空間を高速に処理している．また物理演算の過程はOpenGLを用いて描画している．生成したがれきモデルは，がれきモデルの解析や，レスキューロボットシミュレーションなどに利用できる．"
+      source-address=""
+    />
+  </div>
+</template>
+
+<script>
+import work from './work.vue'
+
+export default {
+  name: 'VirtualGarekiLab',
+  components: {
+    work
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
