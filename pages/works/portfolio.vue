6db7fe0c1c605a0a57d347c0c6f808f8cb7f9a81
Unknown <takuto.hamano@gmail.com>
2018-11-11 21:44:16 +0900

    Update portfolio.vue

--- pages/works/portfolio.vue
+++ pages/works/portfolio.vue
@@ -5,7 +5,7 @@
       :technologies="['HTML/CSS', 'Vue.js', 'Nuxt.js', 'Element UI', 'CircleCI']"
       title="ポートフォリオサイト"
       date="2018/6"
-      description="Nuxt.jsを利用したポートフォリオサイト．UIライブラリとしてElement UIを利用．これまでに作ったものや，撮影した写真を掲載．"
+      description="Nuxt.jsを利用したポートフォリオサイト．UIライブラリとしてElement UIを利用．これまでに作ったものや，撮影した写真を掲載．作成したページは，CircleCIを用いて自動ビルド/デプロイしている．"
       source-address="https://github.com/hama1080/portfolio"
     />
   </div>
